ff1fbbec6bbcd591bfe6c09361a8ba439c38bb04
luizfelipew <luiz.f.campos@partner.bmw.com.br>
2023-05-24 22:17:34 -0300

    add: :sparkles: receiving and developing JSON objects

--- quarkus-social/src/main/java/io/github/luizfw/quarkussocial/rest/UserResource.java
+++ quarkus-social/src/main/java/io/github/luizfw/quarkussocial/rest/UserResource.java
@@ -2,15 +2,21 @@ package io.github.luizfw.quarkussocial.rest;
 
 
 import io.github.luizfw.quarkussocial.rest.dto.CreateUserRequest;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 @Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
 
     @POST
     public Response createUser(final CreateUserRequest userRequest) {
+        return Response.ok(userRequest).build();
+    }
+    @GET
+    public Response listAll() {
         return Response.ok().build();
     }
 }
