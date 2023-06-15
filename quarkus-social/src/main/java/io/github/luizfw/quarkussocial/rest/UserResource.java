f647cae8b521d56c779fd3d7191a8fb6315a5a52
luizfelipew <luiz.f.campos@partner.bmw.com.br>
2023-06-15 10:29:43 -0300

    add: :sparkles: CRUD operation finished

--- quarkus-social/src/main/java/io/github/luizfw/quarkussocial/rest/UserResource.java
+++ quarkus-social/src/main/java/io/github/luizfw/quarkussocial/rest/UserResource.java
@@ -3,12 +3,13 @@ package io.github.luizfw.quarkussocial.rest;
 
 import io.github.luizfw.quarkussocial.domain.model.User;
 import io.github.luizfw.quarkussocial.rest.dto.CreateUserRequest;
-import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+
+import java.util.Objects;
 
 @Path("/users")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -26,11 +27,43 @@ public class UserResource {
 
         return Response.ok(user).build();
     }
+
     @GET
     public Response listAll() {
         PanacheQuery<User> query = User.findAll();
         return Response
                 .ok(query.list())
                 .build();
+    }
+
+    @DELETE
+    @Path("{id}")
+    @Transactional
+    public Response deleteUser(@PathParam("id") Long id) {
+        User user = User.findById(id);
+
+        if (Objects.nonNull(user)) {
+            user.delete();
+            return Response
+                    .ok().build();
+        }
+        return Response
+                .status(Response.Status.NOT_FOUND)
+                .build();
+    }
+
+    @PUT
+    @Path("{id}")
+    @Transactional
+    public Response updateUser(@PathParam("id") Long id, CreateUserRequest userData) {
+        User user = User.findById(id);
+
+        if (Objects.nonNull(user)) {
+            user.setName(userData.getName());
+            user.setAge(userData.getAge());
+            return Response
+                    .ok().build();
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
     }
 }
