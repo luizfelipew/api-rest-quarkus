bdddb7341e373bf57304a7fe34c84587e67afbd9
luizfelipew <luiz.f.campos@partner.bmw.com.br>
2023-09-06 14:04:27 -0300

    add: :sparkles: creating the Posts repository and table

---
+++ quarkus-social/src/main/java/io/github/luizfw/quarkussocial/domain/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package io.github.luizfw.quarkussocial.domain.repository;
+
+import io.github.luizfw.quarkussocial.domain.model.Post;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class PostRepository implements PanacheRepository<Post> {
+}
--- quarkus-social/src/main/java/io/github/luizfw/quarkussocial/rest/PostResource.java
+++ quarkus-social/src/main/java/io/github/luizfw/quarkussocial/rest/PostResource.java
@@ -1,23 +1,63 @@
 package io.github.luizfw.quarkussocial.rest;
 
+import io.github.luizfw.quarkussocial.domain.model.Post;
+import io.github.luizfw.quarkussocial.domain.model.User;
+import io.github.luizfw.quarkussocial.domain.repository.PostRepository;
+import io.github.luizfw.quarkussocial.domain.repository.UserRepository;
+import io.github.luizfw.quarkussocial.rest.dto.CreatePostRequest;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+
+import java.util.Objects;
 
 @Path("/users/{userId}/posts")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class PostResource {
 
+    private UserRepository userRepository;
+    private PostRepository postRepository;
+
+    @Inject
+    public PostResource(UserRepository userRepository, PostRepository postRepository) {
+        this.userRepository = userRepository;
+        this.postRepository = postRepository;
+    }
+
     @POST
-    public Response savePost() {
+    @Transactional
+    public Response savePost(@PathParam("userId") Long userId, CreatePostRequest request) {
+        User user = userRepository.findById(userId);
+
+        if (Objects.isNull(user)) {
+            return Response
+                    .status(Response.Status.NOT_FOUND)
+                    .build();
+        }
+
+        Post post = new Post();
+        post.setText(request.getText());
+        post.setUser(user);
+
+        postRepository.persist(post);
+
         return Response
                 .status(Response.Status.CREATED)
                 .build();
     }
 
     @GET
-    public Response listPosts() {
+    public Response listPosts(@PathParam("userId") Long userId) {
+        User user = userRepository.findById(userId);
+
+        if (Objects.isNull(user)) {
+            return Response
+                    .status(Response.Status.NOT_FOUND)
+                    .build();
+        }
         return Response
                 .ok()
                 .build();
