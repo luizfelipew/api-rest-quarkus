83ae3b7c32ad5a7f3f252886078356abfd1caf5b
luizfelipew <luiz.f.campos@partner.bmw.com.br>
2023-09-11 20:04:37 -0300

    add: :sparkles: implementing follow user method

--- quarkus-social/src/main/java/io/github/luizfw/quarkussocial/rest/FollowerResource.java
+++ quarkus-social/src/main/java/io/github/luizfw/quarkussocial/rest/FollowerResource.java
@@ -1,12 +1,21 @@
 package io.github.luizfw.quarkussocial.rest;
 
+import io.github.luizfw.quarkussocial.domain.model.Follower;
+import io.github.luizfw.quarkussocial.domain.model.User;
 import io.github.luizfw.quarkussocial.domain.repository.FollowerRepository;
 import io.github.luizfw.quarkussocial.domain.repository.UserRepository;
+import io.github.luizfw.quarkussocial.rest.dto.FollowerRequest;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import lombok.val;
+
+import java.util.Objects;
 
 @Path("/users/{userId}/followers")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -15,10 +24,37 @@ public class FollowerResource {
 
     private FollowerRepository followerRepository;
     private UserRepository userRepository;
+    @PathParam("userId")
+    private String userId;
 
     @Inject
     public FollowerResource(FollowerRepository followerRepository, UserRepository userRepository) {
         this.followerRepository = followerRepository;
         this.userRepository = userRepository;
     }
+
+
+    @PUT
+    public Response followUser(@PathParam("userId") Long userId, FollowerRequest request) {
+
+        val user = userRepository.findById(userId);
+        if (Objects.isNull(user)){
+            return Response
+                    .status(Response.Status.NOT_FOUND)
+                    .build();
+        }
+
+        val follower = userRepository.findById(request.getFollowerId());
+
+        val entity = new Follower();
+        entity.setUser(user);
+        entity.setFollower(follower);
+
+        followerRepository.persist(entity);
+
+        return Response
+                .status(Response.Status.NO_CONTENT)
+                .build();
+    }
 }
+
