45222216d386c3e5c5d4901951aa3e9e6e01a72c
luizfelipew <luiz.f.campos@partner.bmw.com.br>
2023-09-11 15:33:27 -0300

    add: :sparkles: adding new table and specific classes in the project

---
+++ quarkus-social/src/main/java/io/github/luizfw/quarkussocial/domain/repository/FollowerRepository.java
@@ -0,0 +1,9 @@
+package io.github.luizfw.quarkussocial.domain.repository;
+
+import io.github.luizfw.quarkussocial.domain.model.Follower;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class FollowerRepository implements PanacheRepository<Follower> {
+}
