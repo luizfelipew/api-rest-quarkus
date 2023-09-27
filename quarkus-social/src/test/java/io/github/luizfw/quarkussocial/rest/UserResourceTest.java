fdb967f5691d9431291e132eddab1c93eaab5a15
luizfelipew <luiz.f.campos@partner.bmw.com.br>
2023-09-27 18:53:21 -0300

    add: :white_check_mark: implementing ordered tests

--- quarkus-social/src/test/java/io/github/luizfw/quarkussocial/rest/UserResourceTest.java
+++ quarkus-social/src/test/java/io/github/luizfw/quarkussocial/rest/UserResourceTest.java
@@ -2,24 +2,35 @@ package io.github.luizfw.quarkussocial.rest;
 
 import io.github.luizfw.quarkussocial.rest.dto.CreateUserRequest;
 import io.github.luizfw.quarkussocial.rest.dto.ResponseError;
+import io.quarkus.test.common.http.TestHTTPResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
-import io.restassured.response.Response;
 import jakarta.json.bind.JsonbBuilder;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
+import java.net.URL;
 import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @QuarkusTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class UserResourceTest {
+
+    @TestHTTPResource("/users")
+    URL apiURL;
 
     @Test
     @DisplayName("should create a user successfully")
+    @Order(1)
     void createUserTest() {
         var user = new CreateUserRequest();
         user.setName("Fulano");
@@ -33,7 +44,7 @@ class UserResourceTest {
                 .contentType(ContentType.JSON)
                 .body(jsonRequest)
                 .when()
-                .post("/users")
+                .post(apiURL)
                 .then()
                 .extract()
                 .response();
@@ -44,6 +55,7 @@ class UserResourceTest {
 
     @Test
     @DisplayName("should return error when json is not valid")
+    @Order(2)
     void createUserValidationErrorTest() {
         var user = new CreateUserRequest();
         user.setName(null);
@@ -57,7 +69,7 @@ class UserResourceTest {
                 .contentType(ContentType.JSON)
                 .body(jsonRequest)
                 .when()
-                .post("/users")
+                .post(apiURL)
                 .then()
                 .extract()
                 .response();
@@ -70,5 +82,18 @@ class UserResourceTest {
         assertNotNull(errors.get(1).get("message"));
 //        assertEquals("Age is Required", errors.get(0).get("message"));
 //        assertEquals("Name is Required", errors.get(1).get("message"));
+    }
+
+    @Test
+    @DisplayName("should list all users")
+    @Order(2)
+    void listAllUsersTest() {
+        given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get(apiURL)
+                .then()
+                .statusCode(200)
+                .body("size()", Matchers.is(1));
     }
 }
